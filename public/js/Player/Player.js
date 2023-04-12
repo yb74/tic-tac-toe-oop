28c16c09fe40c893ba7338c2fefd2eb4708e94f9
yb74 <0622178800yb>
2023-04-12 17:47:43 +0200

    Removing useless variable symbole from Player class

--- public/js/Player/Player.js
+++ public/js/Player/Player.js
@@ -1,7 +1,6 @@
 class Player {
     constructor (playerName, symbol) {
         this.playerName = playerName;
-        this.symbol = symbol;
         this.victory = 0;
         this.defeat = 0;
         this.symboleAcc = [];
